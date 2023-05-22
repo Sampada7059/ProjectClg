f53925264227759c6a017927b1cea2aa789321aa
EI13129 <EI13129@ELW5113>
2023-05-22 13:02:53 +0530

    Add project files.

---
+++ ProjectClg/Controllers/UpdateController.cs
@@ -0,0 +1,28 @@
+﻿using ProjectClg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectClg.Controllers
+{
+    public class UpdateController : Controller
+    {
+        // GET: Update
+        CollegeDBEntities db = new CollegeDBEntities();
+        public ActionResult Update()
+        {
+            
+            return View(db.Proc_GetUpdates().ToList());
+        }
+        [HttpPost]
+        public ActionResult Update(Update u)
+        {
+
+            db.Proc_Updates(u.Information, u.CreatedAdmin);
+            ModelState.Clear();
+            return View(db.Proc_GetUpdates().ToList());
+        }
+    }
+}
---
+++ ProjectClg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectClg.MvcApplication" Language="C#" %>
